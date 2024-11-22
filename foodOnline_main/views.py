69cad65c6bdfed3b52e549567b1ab24e184fae8e
Sudam Wanve <sudam.wanve@infobeans.com>
2024-11-22 11:27:41 +0530

    Home page setup

--- foodOnline_main/views.py
+++ foodOnline_main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('WELCOM tO DJNAGO PROJECT')
+    return render(request,'home.html')
